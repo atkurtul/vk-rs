f7c74beb435c7176ecd0223adda59fd1855b291e
atil <atilkurtulmus@gmail.com>
2020-07-26 15:16:05 +0300

    fix

--- examples/triangle.rs
+++ examples/triangle.rs
@@ -37,28 +37,25 @@ fn main()
 
         let buffer = create_buffer(&vert);
 
-        let mut frame = 0;
         let mut counter = -1i32;
         while win.poll() {
             let mut idx = 0;
             counter = (counter + 1) % 3;
-            frame = counter as _;
+            let frame = counter as usize;
             vk::AcquireNextImageKHR(swapchain, u64::MAX, acquire_semaphore[frame], 0, &mut idx);
             vk::WaitForFences(1, &fence[frame], 1, u64::MAX);
             vk::ResetFences(1, &fence[frame]);
             vk::ResetCommandBuffer(cmd_buffer[frame], vk::COMMAND_BUFFER_RESET_RELEASE_RESOURCES_BIT);
-            let mut cmd_begin_info = vk::CommandBufferBeginInfo::default();
-            cmd_begin_info.flags = vk::COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
-            vk::BeginCommandBuffer(cmd_buffer[frame], &cmd_begin_info);
-
+            vk::BeginCommandBuffer(cmd_buffer[frame], &vk::CommandBufferBeginInfo::new().flags(vk::COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT));
 
             let clear_values = [vk::ClearValue::new().color([0.4, 0.4, 0.5, 1.]), vk::ClearValue::depth(1., 0.) ];
-            let mut pass_info =  vk::RenderPassBeginInfo::default();
-            pass_info.renderPass = renderpass;
-            pass_info.framebuffer = framebuffer[frame];
-            pass_info.renderArea.extent = win.extent(); 
-            pass_info.clearValueCount = 2;
-            pass_info.pClearValues = &clear_values as _;
+            let pass_info =  vk::RenderPassBeginInfo::new()
+                .renderPass(renderpass)
+                .framebuffer(framebuffer[frame])
+                .renderArea(vk::Rect2D::new().extent(win.extent()))
+                .clearValueCount(2)
+                .pClearValues(&clear_values as _);
+
             vk::CmdBeginRenderPass(cmd_buffer[frame], &pass_info, vk::SUBPASS_CONTENTS_INLINE);
 
             vk::CmdBindPipeline(cmd_buffer[frame], vk::PIPELINE_BIND_POINT_GRAPHICS, pipeline);
@@ -68,23 +65,23 @@ fn main()
             vk::CmdEndRenderPass(cmd_buffer[frame]);
             vk::EndCommandBuffer(cmd_buffer[frame]);
 
-            let mut submit_info = vk::SubmitInfo::default();
-            submit_info.waitSemaphoreCount = 1;
-            submit_info.signalSemaphoreCount = 1;
-            submit_info.commandBufferCount = 1;
-            submit_info.pCommandBuffers = &cmd_buffer[frame];
-            submit_info.pWaitDstStageMask = &vk::PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
-            submit_info.pWaitSemaphores = &acquire_semaphore[frame];
-            submit_info.pSignalSemaphores = &present_semaphore[frame];
+            let submit_info = vk::SubmitInfo::new()
+                .waitSemaphoreCount(1)
+                .signalSemaphoreCount(1)
+                .commandBufferCount(1)
+                .pCommandBuffers(&cmd_buffer[frame])
+                .pWaitDstStageMask(&vk::PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT)
+                .pWaitSemaphores(&acquire_semaphore[frame])
+                .pSignalSemaphores(&present_semaphore[frame]);
 
             vk::QueueSubmit(1, &submit_info, fence[frame]);
 
-            let mut present_info = vk::PresentInfoKHR::default();
-            present_info.waitSemaphoreCount = 1;
-            present_info.pWaitSemaphores = &present_semaphore[frame];
-            present_info.swapchainCount = 1;
-            present_info.pSwapchains = &swapchain;
-            present_info.pImageIndices = &counter;
+            let present_info = vk::PresentInfoKHR::new()
+                .waitSemaphoreCount(1)
+                .pWaitSemaphores(&present_semaphore[frame])
+                .swapchainCount(1)
+                .pSwapchains(&swapchain)
+                .pImageIndices(&counter);
             vk::QueuePresentKHR(&present_info);
         }
         
--- examples/util.rs
+++ examples/util.rs
@@ -12,17 +12,17 @@ pub unsafe fn init_vulkan(surface_extension : &str) {
     let extensions_raw : Vec<*const i8> = extensions.iter().map(|s| s.as_ptr()).collect();
     let layers_raw : Vec<*const i8> = layers.iter().map(|s| s.as_ptr()).collect();
     let device_ext_raw : Vec<*const i8> = device_ext.iter().map(|s| s.as_ptr()).collect();
-	let mut appinfo  = vk::ApplicationInfo::default();
-	appinfo.engineVersion = vk::make_version(1, 2, 0);
-	appinfo.applicationVersion = vk::make_version(1, 2, 0);
-    appinfo.apiVersion = vk::make_version(1, 2, 0);
-    
-    let mut info = vk::InstanceCreateInfo::default();
-	info.pApplicationInfo = &appinfo;
-	info.enabledLayerCount = layers_raw.len() as _;
-	info.ppEnabledLayerNames = layers_raw.as_ptr();
-	info.enabledExtensionCount = extensions_raw.len() as _;
-    info.ppEnabledExtensionNames = extensions_raw.as_ptr();
+    let appinfo  = vk::ApplicationInfo::new()
+        .engineVersion(vk::make_version(1, 2, 0))
+	    .applicationVersion(vk::make_version(1, 2, 0))
+        .apiVersion(vk::make_version(1, 2, 0));
+
+    let info = vk::InstanceCreateInfo::new()
+	    .pApplicationInfo(&appinfo)
+	    .enabledLayerCount(layers_raw.len() as _)
+	    .ppEnabledLayerNames(layers_raw.as_ptr())
+	    .enabledExtensionCount(extensions_raw.len() as _)
+        .ppEnabledExtensionNames(extensions_raw.as_ptr());
 
     let res = vk::CreateInstance(&info);
     println!("Instance created {}", res);
@@ -43,29 +43,29 @@ pub unsafe fn init_vulkan(surface_extension : &str) {
     println!("Found device {}", pdev);
     
     let prio = 1f32;
-	let mut qinfo = vk::DeviceQueueCreateInfo::default();
-	qinfo.queueCount = 1;
-	qinfo.pQueuePriorities = &prio;
+	let qinfo = vk::DeviceQueueCreateInfo::new()
+	    .queueCount(1)
+	    .pQueuePriorities(&prio);
 
-    let mut features = vk::PhysicalDeviceFeatures::default();
-	features.fillModeNonSolid = 1;
-	features.sampleRateShading = 1;
-	features.vertexPipelineStoresAndAtomics = 1;
-    features.samplerAnisotropy = 1;
+    let features = vk::PhysicalDeviceFeatures::new()
+	    .fillModeNonSolid(1)
+	    .sampleRateShading(1)
+	    .vertexPipelineStoresAndAtomics(1)
+        .samplerAnisotropy(1);
     
-    let mut ext_features = vk::PhysicalDeviceDescriptorIndexingFeatures::default();
-	ext_features.descriptorBindingPartiallyBound = 1;
-    ext_features.descriptorBindingVariableDescriptorCount = 1;
+    let ext_features = vk::PhysicalDeviceDescriptorIndexingFeatures::new()
+	    .descriptorBindingPartiallyBound(1)
+        .descriptorBindingVariableDescriptorCount(1);
     
-    let mut device_info = vk::DeviceCreateInfo::default();
-	device_info.queueCreateInfoCount = 1;
-	device_info.pNext = &ext_features as *const _ as _;
-	device_info.pQueueCreateInfos = &qinfo;
-	device_info.pEnabledFeatures = &features;
-	device_info.enabledExtensionCount = device_ext_raw.len() as _;
-	device_info.ppEnabledExtensionNames = device_ext_raw.as_ptr();
-	device_info.enabledLayerCount = layers_raw.len() as _;
-    device_info.ppEnabledLayerNames = layers_raw.as_ptr();
+    let device_info = vk::DeviceCreateInfo::new()
+        .queueCreateInfoCount(1)
+	    .pNext(&ext_features as *const _ as _)
+	    .pQueueCreateInfos(&qinfo)
+	    .pEnabledFeatures(&features)
+	    .enabledExtensionCount(device_ext_raw.len() as _)
+	    .ppEnabledExtensionNames(device_ext_raw.as_ptr())
+	    .enabledLayerCount(layers_raw.len() as _)
+        .ppEnabledLayerNames(layers_raw.as_ptr());
     
     let res = vk::CreateDevice(&device_info);
     vk::SetDeviceQueue(0, 0);
@@ -74,19 +74,19 @@ pub unsafe fn init_vulkan(surface_extension : &str) {
 
 pub unsafe fn create_swapchain(extent : vk::Extent2D, surface : vk::SurfaceKHR) -> vk::SwapchainKHR {
     use vk::*;
-    let mut info = SwapchainCreateInfoKHR::default();
-    info.surface = surface;
-    info.minImageCount = 3;
-    info.imageFormat = FORMAT_B8G8R8A8_UNORM;
-    info.imageColorSpace = COLOR_SPACE_SRGB_NONLINEAR_KHR;
-    info.imageExtent = extent;
-    info.imageArrayLayers = 1;
-    info.imageUsage = IMAGE_USAGE_COLOR_ATTACHMENT_BIT as _;
-    info.imageSharingMode = SHARING_MODE_EXCLUSIVE;
-    info.preTransform = SURFACE_TRANSFORM_IDENTITY_BIT_KHR;
-    info.compositeAlpha = COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
-    info.presentMode = PRESENT_MODE_IMMEDIATE_KHR;
-    info.clipped = 1;
+    let info = SwapchainCreateInfoKHR::new()
+        .surface(surface)
+        .minImageCount(3)
+        .imageFormat(FORMAT_B8G8R8A8_UNORM)
+        .imageColorSpace(COLOR_SPACE_SRGB_NONLINEAR_KHR)
+        .imageExtent(extent)
+        .imageArrayLayers(1)
+        .imageUsage(IMAGE_USAGE_COLOR_ATTACHMENT_BIT as _)
+        .imageSharingMode(SHARING_MODE_EXCLUSIVE)
+        .preTransform(SURFACE_TRANSFORM_IDENTITY_BIT_KHR)
+        .compositeAlpha(COMPOSITE_ALPHA_OPAQUE_BIT_KHR)
+        .presentMode(PRESENT_MODE_IMMEDIATE_KHR)
+        .clipped(1);
     let mut swapchain = 0;
     let result = CreateSwapchainKHR(&info, &mut swapchain);
     println!("Swapchain created {}", result);
@@ -150,9 +150,9 @@ pub fn make_renderpass(ms : i32) -> vk::RenderPass{
         make_depth_attachment(1),
     ];
     let refs = [ make_color_ref(0), make_depth_ref(1) ];
-    let mut subpass =  make_subpass_desc(1);
-    subpass.pColorAttachments = &refs[0];
-    subpass.pDepthStencilAttachment = &refs[1];
+    let subpass =  make_subpass_desc(1)
+        .pColorAttachments(&refs[0])
+        .pDepthStencilAttachment(&refs[1]);
 
     let info = vk::RenderPassCreateInfo {
         attachmentCount : attachments.len() as _,
@@ -168,6 +168,9 @@ pub fn make_renderpass(ms : i32) -> vk::RenderPass{
 
 pub fn create_pipeline(pass : vk::RenderPass) -> vk::Pipeline {
     unsafe {
+        let layout_info = vk::PipelineLayoutCreateInfo::new();
+        let mut layout = 0;
+        vk::CreatePipelineLayout(&layout_info, &mut layout);
         let stage = stage_info();
         let input_state = vertex_input_state();
         let assembler = assembler_info();
@@ -177,83 +180,67 @@ pub fn create_pipeline(pass : vk::RenderPass) -> vk::Pipeline {
         let depth = depth_info();
         let blend = blend_info();
         let mut pipe : vk::Pipeline = 0;
-        let mut info = vk::GraphicsPipelineCreateInfo::default();
-        info.stageCount = 2;
-        info.pStages = stage.0.as_ptr();
-        info.pVertexInputState = &input_state.0;
-        info.pInputAssemblyState = &assembler;
-        info.pViewportState = &viewport.0;
-        info.pRasterizationState = &rasterizer;
-        info.pMultisampleState = &ms;
-        info.pDepthStencilState = &depth;
-        info.pColorBlendState = &blend.0;
-        info.renderPass = pass;
-        let layout_info = vk::PipelineLayoutCreateInfo::default();
-        vk::CreatePipelineLayout(&layout_info, &mut info.layout);
+        let info = vk::GraphicsPipelineCreateInfo::new()
+            .stageCount(2)
+            .pStages(stage.0.as_ptr())
+            .pVertexInputState(&input_state.0)
+            .pInputAssemblyState(&assembler)
+            .pViewportState(&viewport.0)
+            .pRasterizationState(&rasterizer)
+            .pMultisampleState(&ms)
+            .pDepthStencilState(&depth)
+            .pColorBlendState(&blend.0)
+            .renderPass(pass)
+            .layout(layout);
         vk::CreateGraphicsPipelines(0, 1, &info, &mut pipe);
         pipe
     }
 }
 
 fn blend_info() -> (vk::PipelineColorBlendStateCreateInfo, Box<vk::PipelineColorBlendAttachmentState>) {
-	let mut info = vk::PipelineColorBlendStateCreateInfo::default();
-	let mut attachment = Box::new(vk::PipelineColorBlendAttachmentState::default());
-	info.attachmentCount = 1;
-	info.pAttachments = &*attachment;
-	attachment.colorWriteMask = (vk::COLOR_COMPONENT_R_BIT |
-    vk::COLOR_COMPONENT_G_BIT | vk::COLOR_COMPONENT_B_BIT | vk::COLOR_COMPONENT_A_BIT) as _;
-    (info, attachment)
+    let attachment = Box::new(vk::PipelineColorBlendAttachmentState::new()
+        .colorWriteMask((vk::COLOR_COMPONENT_R_BIT |
+        vk::COLOR_COMPONENT_G_BIT | vk::COLOR_COMPONENT_B_BIT | vk::COLOR_COMPONENT_A_BIT) as _));
+    (vk::PipelineColorBlendStateCreateInfo::new()
+    .attachmentCount(1)
+    .pAttachments(&*attachment), attachment)
 }
 
 fn depth_info() -> vk::PipelineDepthStencilStateCreateInfo {
-	let mut info = vk::PipelineDepthStencilStateCreateInfo::default();
-	info.depthTestEnable = 1;
-	info.depthWriteEnable = 1;
-	info.depthCompareOp = vk::COMPARE_OP_LESS;
-	info
+	vk::PipelineDepthStencilStateCreateInfo::new()
+	    .depthTestEnable(1)
+	    .depthWriteEnable(1)
+	    .depthCompareOp(vk::COMPARE_OP_LESS)
 }
 
 fn ms_info() -> vk::PipelineMultisampleStateCreateInfo {
-	let mut info = vk::PipelineMultisampleStateCreateInfo::default();
-	info.rasterizationSamples = 1;
-	info
+	vk::PipelineMultisampleStateCreateInfo::new().rasterizationSamples(1)
 }
 
 fn rasterizer_info() -> vk::PipelineRasterizationStateCreateInfo{
-	let mut info = vk::PipelineRasterizationStateCreateInfo::default();
-	info.polygonMode = vk::POLYGON_MODE_FILL;
-	info.lineWidth = 1f32;
-	info.frontFace = vk::FRONT_FACE_CLOCKWISE;
-	info.cullMode = vk::CULL_MODE_NONE as _;
-    info
+	vk::PipelineRasterizationStateCreateInfo::new()
+	    .polygonMode(vk::POLYGON_MODE_FILL)
+	    .lineWidth(1f32)
+	    .frontFace(vk::FRONT_FACE_CLOCKWISE)
+	    .cullMode(vk::CULL_MODE_NONE as _)
 }
 
 fn viewport_info(width : u32, height : u32) -> 
         (vk::PipelineViewportStateCreateInfo,
         Box<vk::Viewport>,
         Box<vk::Rect2D>) {
-	let mut viewport = Box::new(vk::Viewport::default());
-	let mut scissor = Box::new(vk::Rect2D::default());
+	let viewport = Box::new(vk::Viewport::new().width(width as _).height(height as _).maxDepth(1.));
+	let scissor = Box::new(vk::Rect2D::new().extent(vk::Extent2D{width, height}));
 
-	viewport.width	= width as _;
-	viewport.height	= height as _;
-	viewport.maxDepth = 1.;
-
-	scissor.extent = vk::Extent2D { width, height };
-
-	let mut info = vk::PipelineViewportStateCreateInfo::default();
-	info.viewportCount = 1;
-	info.scissorCount = 1;
-	info.pScissors = &*scissor;
-	info.pViewports = &*viewport;
-
-	(info, viewport, scissor)
+	(vk::PipelineViewportStateCreateInfo::new()
+	    .viewportCount(1)
+	    .scissorCount(1)
+	    .pScissors(&*scissor)
+	    .pViewports(&*viewport), viewport, scissor)
 }
 
 fn assembler_info() -> vk::PipelineInputAssemblyStateCreateInfo {
-	let mut info = vk::PipelineInputAssemblyStateCreateInfo::default();
-	info.topology = vk::PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
-	return info;
+	vk::PipelineInputAssemblyStateCreateInfo::new().topology(vk::PRIMITIVE_TOPOLOGY_TRIANGLE_LIST)
 }
 
 fn vertex_input_state() -> (vk::PipelineVertexInputStateCreateInfo, 
@@ -316,18 +303,16 @@ pub fn create_resources(swapchain : vk::SwapchainKHR, renderpass : vk::RenderPas
         let mut views = vec![0u64;3];
         let mut framebuffer = vec![0u64;3];
 
-        let mut pool_info = vk::CommandPoolCreateInfo::default();
-        pool_info.flags = vk::COMMAND_POOL_CREATE_TRANSIENT_BIT | vk::COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;   
-        vk::CreateCommandPool(&pool_info, &mut pool);
+        vk::CreateCommandPool(&vk::CommandPoolCreateInfo::new()
+            .flags(vk::COMMAND_POOL_CREATE_TRANSIENT_BIT | vk::COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT), &mut pool);
 
-        let mut alloc_info = vk::CommandBufferAllocateInfo::default();
-        alloc_info.commandPool = pool;
-        alloc_info.level = vk::COMMAND_BUFFER_LEVEL_PRIMARY;
-        alloc_info.commandBufferCount = 3;
+        let alloc_info = vk::CommandBufferAllocateInfo::new()
+            .commandPool(pool)
+            .level(vk::COMMAND_BUFFER_LEVEL_PRIMARY)
+            .commandBufferCount(3);
+
         vk::AllocateCommandBuffers(&alloc_info, cmd_buffers.as_mut_ptr());
-        let mut fence_info = vk::FenceCreateInfo::default();
-        fence_info.flags = vk::FENCE_CREATE_SIGNALED_BIT;
-        fence.iter_mut().for_each(|f| {vk::CreateFence(&fence_info, f);});
+        fence.iter_mut().for_each(|f| {vk::CreateFence(&vk::FenceCreateInfo::new().flags(vk::FENCE_CREATE_SIGNALED_BIT), f);});
         let semaphore_info = vk::SemaphoreCreateInfo::default();
         acquire_semaphore.iter_mut().for_each(|s| { vk::CreateSemaphore(&semaphore_info, s); });
         present_semaphore.iter_mut().for_each(|s| { vk::CreateSemaphore(&semaphore_info, s); });
@@ -347,18 +332,16 @@ pub fn create_resources(swapchain : vk::SwapchainKHR, renderpass : vk::RenderPas
 }
 
 fn create_image(format : vk::Format, usage : vk::ImageUsageFlags, width : u32, height : u32) -> vk::Image {
-	let mut info = vk::ImageCreateInfo::default();
-	info.imageType = vk::IMAGE_TYPE_2D;
-	info.extent.width = width;
-	info.extent.height = height;
-	info.extent.depth = 1;
-	info.mipLevels = 1;
-	info.arrayLayers = 1;
-	info.tiling = vk::IMAGE_TILING_OPTIMAL;
-	info.sharingMode = vk::SHARING_MODE_EXCLUSIVE;
-	info.samples = 1;
-	info.format = format;
-    info.usage = usage; 
+	let info = vk::ImageCreateInfo::new()
+	    .imageType(vk::IMAGE_TYPE_2D)
+	    .extent(vk::Extent3D{width, height, depth : 1})
+	    .mipLevels(1)
+	    .arrayLayers(1)
+	    .tiling(vk::IMAGE_TILING_OPTIMAL)
+	    .sharingMode(vk::SHARING_MODE_EXCLUSIVE)
+	    .samples(1)
+	    .format(format)
+        .usage(usage);
     let mut image = 0;
     unsafe { vk::CreateImage(&info, &mut image); }
 	image
@@ -366,7 +349,7 @@ fn create_image(format : vk::Format, usage : vk::ImageUsageFlags, width : u32, h
 
 fn find_memory_type_index(req : vk::MemoryRequirements, flags : vk::MemoryPropertyFlags) -> u32 
 {
-    let mut props = vk::PhysicalDeviceMemoryProperties::default();
+    let mut props = <_>::default();
     unsafe { vk::GetPhysicalDeviceMemoryProperties(&mut props); }
     props.memoryTypes.iter().enumerate().find(|t| (t.1.propertyFlags & flags) == flags).unwrap().0 as _
 }
@@ -375,9 +358,9 @@ fn alloc_image_memory(image : vk::Image) -> vk::DeviceMemory {
     unsafe {
         let mut req = vk::MemoryRequirements::default();
         vk::GetImageMemoryRequirements(image, &mut req);
-        let mut info = vk::MemoryAllocateInfo::default();
-        info.allocationSize = req.size;
-        info.memoryTypeIndex = find_memory_type_index(req, vk::MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+        let info = vk::MemoryAllocateInfo::new()
+            .allocationSize(req.size)
+            .memoryTypeIndex(find_memory_type_index(req, vk::MEMORY_PROPERTY_DEVICE_LOCAL_BIT));
         let mut memory = 0;
         vk::AllocateMemory(&info, &mut memory);
         vk::BindImageMemory(image, memory, 0);
@@ -387,13 +370,14 @@ fn alloc_image_memory(image : vk::Image) -> vk::DeviceMemory {
 
 fn create_image_view(img : vk::Image, format : vk::Format, aspect : vk::ImageAspectFlags) -> vk::ImageView
 {
-	let mut info = vk::ImageViewCreateInfo::default();
-	info.image = img;
-	info.viewType = vk::IMAGE_VIEW_TYPE_2D;
-	info.format = format;
-	info.subresourceRange.aspectMask = aspect;
-	info.subresourceRange.levelCount = 1;
-    info.subresourceRange.layerCount = 1;
+	let info = vk::ImageViewCreateInfo::new()
+	    .image(img)
+	    .viewType(vk::IMAGE_VIEW_TYPE_2D)
+	    .format(format)
+	    .subresourceRange(
+            vk::ImageSubresourceRange::new().aspectMask(aspect)
+	        .levelCount(1)
+            .layerCount(1));
     let mut view = 0;
     unsafe { vk::CreateImageView(&info, &mut view); }
     view
@@ -409,13 +393,13 @@ fn create_image_and_view(format : vk::Format, usage : vk::ImageUsageFlags, width
 
 fn create_framebuffer(pass : vk::RenderPass, attachments : Vec<vk::ImageView>, width : u32, height : u32) -> vk::Framebuffer
 {
-	let mut info = vk::FramebufferCreateInfo::default();
-	info.renderPass = pass;
-	info.attachmentCount = attachments.len() as _;
-	info.pAttachments = attachments.as_ptr();
-	info.width = width;
-	info.height = height;
-    info.layers = 1;
+	let info = vk::FramebufferCreateInfo::new()
+	    .renderPass(pass)
+	    .attachmentCount(attachments.len() as _)
+	    .pAttachments(attachments.as_ptr())
+	    .width(width)
+	    .height(height)
+        .layers(1);
     let mut framebuffer = 0;
     unsafe { vk::CreateFramebuffer(&info, &mut framebuffer); }
     framebuffer
@@ -425,10 +409,11 @@ fn alloc_buffer_memory(buffer : vk::Buffer) -> (vk::DeviceMemory, *mut f32) {
     unsafe {
         let mut req = vk::MemoryRequirements::default();
         vk::GetBufferMemoryRequirements(buffer, &mut req);
-        let mut info = vk::MemoryAllocateInfo::default();
-        info.allocationSize = req.size;
-        info.memoryTypeIndex = find_memory_type_index(req, vk::MEMORY_PROPERTY_HOST_VISIBLE_BIT |
-                 vk::MEMORY_PROPERTY_HOST_COHERENT_BIT);
+        let info = vk::MemoryAllocateInfo::new()
+            .allocationSize(req.size)
+            .memoryTypeIndex(
+                    find_memory_type_index(req,  vk::MEMORY_PROPERTY_HOST_VISIBLE_BIT |
+                                                        vk::MEMORY_PROPERTY_HOST_COHERENT_BIT));
         let mut memory = 0;
         vk::AllocateMemory(&info, &mut memory);
         vk::BindBufferMemory(buffer, memory, 0);
@@ -440,10 +425,10 @@ fn alloc_buffer_memory(buffer : vk::Buffer) -> (vk::DeviceMemory, *mut f32) {
 
 pub fn create_buffer(vert : &Vec<f32>) -> (vk::Buffer, vk::DeviceMemory, *mut f32) {
     unsafe {
-        let mut info = vk::BufferCreateInfo::default();
-        info.usage = vk::BUFFER_USAGE_VERTEX_BUFFER_BIT;
-        info.sharingMode = vk::SHARING_MODE_EXCLUSIVE;
-        info.size = (vert.len() * 4usize) as _;
+        let info = vk::BufferCreateInfo::new()
+            .usage(vk::BUFFER_USAGE_VERTEX_BUFFER_BIT)
+            .sharingMode(vk::SHARING_MODE_EXCLUSIVE)
+            .size((vert.len() * 4usize) as _);
         let mut buffer = 0;
         vk::CreateBuffer(&info, &mut buffer); 
         let memory = alloc_buffer_memory(buffer);
